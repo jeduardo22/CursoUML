8a2f5605ee485d7baa0a75ccfb60395cdec0974e
jeduardo22 <jeduardo.miranda@gmail.com>
2021-02-14 20:39:32 -0300

    Projeto criado

---
+++ src/test/java/com/lototarget/cursoUml/CursoUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lototarget.cursoUml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
